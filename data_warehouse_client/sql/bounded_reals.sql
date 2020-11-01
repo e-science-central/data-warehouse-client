2dfd82dc0c59bfafe5753be70ef53d3e707147bb
benwatson528 <benwatson528@gmail.com>
2020-11-01 15:08:14 +0000

    Substituting one file

---
+++ data_warehouse_client/sql/bounded_reals.sql
@@ -0,0 +1,17 @@
+SELECT
+  DISTINCT measurement.id
+FROM
+  measurement
+  JOIN boundsreal ON (
+    measurement.measurementtype = boundsreal.measurementtype
+    AND measurement.study = boundsreal.study
+  )
+WHERE
+  measurement.valtype = 8
+  AND measurement.study = $study
+  AND (
+    measurement.valreal < boundsreal.minval
+    OR measurement.valreal > boundsreal.maxval
+  )
+ORDER BY
+  measurement.id;
