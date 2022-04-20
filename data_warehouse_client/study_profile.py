33ad5cddbb984a832ef358d4c7d480adcdf383c6
Paul Watson <PaulWatson9994@users.noreply.github.com>
2022-04-20 22:07:17 +0100

    add options to profiling

--- data_warehouse_client/study_profile.py
+++ data_warehouse_client/study_profile.py
@@ -16,29 +16,54 @@
 import pandas as pd
 import pandas_profiling as pp
 import datetime
-from data_warehouse_client import data_warehouse
 
 
 def mk_html_report_file_name(f_dir, report_name, time_string):
     return f_dir + report_name + time_string + ".html"
 
 
-def profile_all_measurement_groups(dw, study, report_dir):
+def profile_all_measurement_groups(dw, study, report_dir, select_participants=False, participants=[],
+                                   select_trials=False, trials=[], hide_trial_column=False):
     """
     Write a Profile file for each measurement group in a study
     :param dw: data warehouse handle
     :param study: study id
     :param report_dir: the directory in which the profiles will be written
+    :param select_participants: select a subset of participants to be included in the profile
+    :param participants: list of participants to be included in the profile if select_participants is true
+    :param select_trials: select a subset of trials to be included in the profile
+    :param trials: list of trials to be included in the profile if select_trials is true
+    :param hide_trial_column: don't include the Trial column (useful for studies where trial is not used)
     """
-
     timestamp = datetime.datetime.now()                        # use the current date and time in the filenames
     time_fname_str = timestamp.strftime('%Y-%m-%dh%Hm%Ms%S')   # format the date time string used in the filename
     measurement_groups = dw.get_all_measurement_groups(study)  # get all the measurement groups in the study
 
     for [mg_id, mg_name] in measurement_groups:                # for each measurement group
         (header, instances) = dw.get_measurement_group_instances(study, mg_id, [])   # extract header and all instances
-        df = pd.DataFrame(instances, columns=header)                                 # create a pandas data frame
-        profile = pp.ProfileReport(df, title=mg_name + " Pandas Profiling Report")   # create a profile report
+        df_full = pd.DataFrame(instances, columns=header)                            # create a pandas data frame
+        if select_participants:   # select participants
+            df_participants = df_full.loc[df_full['Participant'] in participants]
+        else:
+            df_participants = df_full   # include all participants
+        if select_trials:  # select trials
+            df = df_participants.loc[df_participants['Trial'] in trials]
+        else:
+            df = df_participants   # include all trials
+        # drop columns that are not needed in the profile
+        if hide_trial_column:
+            std_columns_to_drop = ['Trial', 'Instance', 'Study', 'Measurement Group', 'Participant']
+        else:
+            std_columns_to_drop = ['Instance', 'Study', 'Measurement Group', 'Participant']
+        if 'uniqueId' in header:
+            columns_to_drop = ['uniqueId'] + std_columns_to_drop
+        else:
+            if 'uniqueid' in header:
+                columns_to_drop = ['uniqueid'] + std_columns_to_drop
+            else:
+                columns_to_drop = std_columns_to_drop
+        df_main_columns = df.drop(columns=columns_to_drop)
+        # create a profile report
+        profile = pp.ProfileReport(df_main_columns, title="Profiling Report for " + mg_name, progress_bar=True)
         f_name = mk_html_report_file_name(report_dir, "Measurement-Group-Profile-" + mg_name + "-", time_fname_str)
         profile.to_file(f_name)  # store the report in  a file
-
